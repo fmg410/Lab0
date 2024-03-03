f70f21e52dab7ac1f9d63d2bf657386ddb430c1d
fmg410 <fmg4@wp.pl>
2024-03-03 23:56:12 +0100

    Add project files.

---
+++ Lab0/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab0
+{
+    internal class FizzBuzz
+    {
+        private int _max;
+
+        public FizzBuzz(int max)
+        {  _max = max; }
+
+        public void func()
+        {
+            for (int i = 1; i <= _max; i++)
+            {
+                if (i % 3 == 0 && i % 5  == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Lab0/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz f = new FizzBuzz(20);
+            f.func();
+        }
+    }
+}
